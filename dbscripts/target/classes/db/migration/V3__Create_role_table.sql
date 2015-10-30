4c8b366426fb908e8a16eec2aa32a4c08b6dafbe
hojha <harshvardhanojha@mindagroup.com>
2015-10-30 23:06:41 +0530

    added cors support from backend

--- dbscripts/target/classes/db/migration/V3__Create_role_table.sql
+++
@@ -1,7 +0,0 @@
-create table role (
-    id bigint primary key not null auto_increment,
-    name varchar(255) not null,
-    code varchar(255) not null,
-    createdon datetime not null,
-    updatedon datetime
-) engine=innodb;
