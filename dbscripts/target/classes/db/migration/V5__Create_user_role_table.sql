13567302c54123a4d970d6823f34835c635288ba
hojha <harshvardhanojha@mindagroup.com>
2015-11-16 21:07:29 +0530

    deleted target

--- dbscripts/target/classes/db/migration/V5__Create_user_role_table.sql
+++
@@ -1,5 +0,0 @@
-create table user_role (
-    id bigint primary key not null auto_increment,
-    roles_id bigint not null references role(id),
-    user_id bigint not null references user(id)
-) engine=innodb;
